dcdeabbd004e379ef185852fc99d4990c27c5b34
OLEKSANDR RASHEVCHENKO <rashevchenkoo@gmail.com>
2024-04-21 16:34:48 +0200

    done

--- src/test/java/oAuth/OAuthTest.java
+++ src/test/java/oAuth/OAuthTest.java
@@ -1,11 +1,20 @@
 package oAuth;
 
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import pojo.Api;
+import pojo.GetCourse;
+import pojo.WebAutomation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
 public class OAuthTest {
     public static void main(String[] args) {
+        String[] courseTitles={"Selenium Webdriver Java","Cypress","Protractor"};
         String response=given()
                 .formParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                 .formParams("client_secret","erZOWM9g3UtwNRj340YYaK_W")
@@ -17,10 +26,28 @@ public class OAuthTest {
         JsonPath js = new JsonPath(response);
         String accessToken= js.getString("access_token");
 
-        String responseTwo= given()
+        GetCourse gc= given()
                 .queryParams("access_token",accessToken)
-                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails?access_token=t/NDoTTj2B2ut6DC1WEkDQ==").asString();
-        System.out.println(responseTwo);
+                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourse.class);
+        System.out.println(gc.getLinkedIn());
+        System.out.println(gc.getInstructor());
+        System.out.println(gc.getCourses().getApi().get(1).getCourseTitle());
+
+        List<Api> apiCourses=gc.getCourses().getApi();
+        for (int i=0;i<apiCourses.size();i++){
+            if (apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing")){
+                System.out.println(apiCourses.get(i).getPrice());
+            }
+        }
+
+        //Get the courseName of Web automation
+        ArrayList<String> a = new ArrayList<String>();
+        List<WebAutomation>w=gc.getCourses().getWebAutomation();
+        for (int j=0;j<w.size();j++){
+            a.add(w.get(j).getCourseTitle());
+        }
+        List<String> expectedList=Arrays.asList(courseTitles);
+        Assert.assertTrue(a.equals(expectedList));
 
     }
 }
